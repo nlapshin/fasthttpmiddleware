4aea4c1df4b877cba7ecc91710914d8abe91b604
Nikolay Lapshin <nlapshin1989@gmail.com>
2021-07-18 14:10:51 +0300

    Added middleware module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module fasthttp-middelware
+
+go 1.16
+
+require github.com/valyala/fasthttp v1.28.0 // indirect
---
+++ main.go
@@ -0,0 +1,33 @@
+package fasthttpmiddelware
+
+import "github.com/valyala/fasthttp"
+
+type MiddlewareInstance func(reqHandler fasthttp.RequestHandler) fasthttp.RequestHandler
+
+type Middleware struct {
+	list []MiddlewareInstance
+}
+
+func New() *Middleware {
+	var list []MiddlewareInstance
+
+	return &Middleware{
+		list,
+	}
+}
+
+func (middleware *Middleware) Use(instance MiddlewareInstance) {
+	middleware.list = append(middleware.list, instance)
+}
+
+func (middleware *Middleware) Handler(reqHandler fasthttp.RequestHandler) fasthttp.RequestHandler {
+	for i := range middleware.list {
+		reqHandler = middleware.list[i](reqHandler)
+
+		if reqHandler == nil {
+			break
+		}
+	}
+
+	return reqHandler
+}
